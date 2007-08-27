fdfd84e61021f8dc3531dc36198993c7c716beb8
ralfoide <ralfoide@08a6a04f-b138-0410-82ad-c55551ec1e38>
2007-08-27 17:12:35 +0000

    Design idea, skeleton

---
+++ src/rig3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------|
+"""
+Rig3 main module.
+
+License GPL.
+
+"""
+
+__author__ = "ralfoide@gmail.com"
+
+#------------------------
+
+#------------------------
+if __name__ == "__main__":
+  main()
+
+
+#------------------------
+# Local Variables:
+# mode: python
+# tab-width: 2
+# py-continuation-offset: 2
+# py-indent-offset: 2
+# py-master-file: "rig2serv.py"
+# sentence-end-double-space: nil
+# fill-column: 79
+# End:
