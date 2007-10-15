6c48d16e3df9642ebf0650f1e9f8c3b054a324aa
ralfoide <ralfoide@08a6a04f-b138-0410-82ad-c55551ec1e38>
2007-10-15 06:42:50 +0000

    Test the kid templating system.

---
+++ rig3serv/src/tests/test_kid.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------|
+"""
+Unit tests for Kid
+
+Part of Rig3.
+License GPL.
+"""
+__author__ = "ralfoide@gmail.com"
+
+import os
+
+from tests.rig_test_case import RigTestCase
+from rig.empty import Empty
+
+#------------------------
+class EmptyTest(RigTestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testSimpleKidTemplate(self):
+        """
+        Test we can render something simple using the Kid templating system.
+        
+        If not present, get it here: http://www.kid-templating.org/
+        """
+        import kid
+        filename = os.path.join(self.getTestDataPath(), "simple_kid.xml")
+        template = kid.Template(filename, foo="MyFoo")
+        template.bar = "MyBar"
+        result = template.serialize()
+        expected = """<?xml version="1.0" encoding="utf-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Test-Title</title>
+</head>
+<body>
+  Kid Test.
+  Title is Test-Title.
+  Title is <span>TEST-TITLE</span>.
+  <div>TEST-TITLE</div>.
+  From caller: MyFoo, MyBar.
+</body>
+</html>"""
+        self.assertEquals(expected, result,
+                          "HTML differs:\nExpected: %s\nActual  : %s\n" % (
+                              repr(expected), repr(result)))
+
+
+#------------------------
+# Local Variables:
+# mode: python
+# tab-width: 4
+# py-continuation-offset: 4
+# py-indent-offset: 4
+# sentence-end-double-space: nil
+# fill-column: 79
+# End:
