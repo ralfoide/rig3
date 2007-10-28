f952ff1a56e52126e9c6787d322062a616656106
ralfoide <ralfoide@08a6a04f-b138-0410-82ad-c55551ec1e38>
2007-10-28 17:42:05 +0000

    Rig3 Live test

---
+++ rig3serv/src/tests/z_last/test_rig3_live.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------|
+"""
+Unit tests for Rig3 site generation
+
+Part of Rig3.
+License GPL.
+"""
+__author__ = "ralfoide@gmail.com"
+
+import os
+
+from tests.rig_test_case import RigTestCase
+from rig3 import Rig3
+
+_DEST_DIR = "live_dest"  # in testdat dir
+
+#------------------------
+class Rig3LiveTest(RigTestCase):
+
+    def setUp(self):
+        self._pwd = os.getcwd()
+        self._testdata = self.getTestDataPath()
+        # all paths are relative to the testdata dir
+        os.chdir(self._testdata)
+        # cleanup output and make sure it's not there anymore
+        if os.path.exists(_DEST_DIR):
+            os.removedirs(_DEST_DIR)
+        self.assertFalse(os.path.exists(_DEST_DIR))
+        self.m = Rig3()
+
+    def tearDown(self):
+        os.chdir(self._pwd)
+        self.m = None
+
+    def testLive(self):
+        rc = os.path.join(self._testdata, "z_last_rig3_live.rc")
+        args = [ "rig3", "-c", rc ]
+        self.m.ParseArgs(args)
+        self.m.Run()
+        self.m.Close()
+
+
+#------------------------
+# Local Variables:
+# mode: python
+# tab-width: 4
+# py-continuation-offset: 4
+# py-indent-offset: 4
+# sentence-end-double-space: nil
+# fill-column: 79
+# End:
