d4f4edb37231f9976a8970f34e28beef0bf07d02
ralfoide <ralfoide@08a6a04f-b138-0410-82ad-c55551ec1e38>
2010-02-11 05:56:23 +0000

    Tweak post page generation to avoid absolute links

--- rig3serv/src/rig/version.py
+++ rig3serv/src/rig/version.py
@@ -13,7 +13,7 @@ Note: The following SVN keywords are enabled for substitution:
 To enable substitutions, do something like this:
   $ svn propset svn:keywords "Date Author Revision HeadURL Id" version.py
 
------
+------
 
 Part of Rig3.
 Copyright (C) 2007-2009 ralfoide gmail com
